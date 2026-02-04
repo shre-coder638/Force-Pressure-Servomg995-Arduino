6472f63dffc8bb086d7bfea4121074ceaa51756a
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-04 13:40:21 +0530

    Pin setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <Servo.h>
-
+Servo myServo;
 
 void setup() {
+    Serial.begin(9600);
+    myServo.attach(11);
+    Serial.println("Servo initiate");
 // write your initialization code here
 }
 
